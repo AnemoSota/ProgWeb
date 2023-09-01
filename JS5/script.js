9e76396cea358bc47a1e353640a87ffd268cd6d5
Atos <atos.omena@icomp.ufam.edu.br>
2023-08-31 20:57:31 -0400

    Atividades 2 a 5 de JS

---
+++ JS5/script.js
@@ -0,0 +1,22 @@
+
+function barras(){
+    let n1 = document.entrada.n1.value;
+    let n2 = document.entrada.n2.value;
+    let n3 = document.entrada.n3.value;
+    let n4 = document.entrada.n4.value;
+    let n5 = document.entrada.n5.value;
+    let n6 = document.entrada.n6.value;
+    // console.log(n1);
+    // console.log(n2);
+    // console.log(n3);
+    // console.log(n4);
+    // console.log(n5);
+    // console.log(n6);
+
+    document.getElementById("n1").style.cssText = "height: "+n1+"px; width:"+n6+"px; background-color:red;";
+    document.getElementById("n2").style.cssText = "height: "+n2+"px; width:"+n6+"px; background-color:red;";
+    document.getElementById("n3").style.cssText = "height: "+n3+"px; width:"+n6+"px; background-color:red;";
+    document.getElementById("n4").style.cssText = "height: "+n4+"px; width:"+n6+"px; background-color:red;";
+    document.getElementById("n5").style.cssText = "height: "+n5+"px; width:"+n6+"px; background-color:red;";
+
+}
